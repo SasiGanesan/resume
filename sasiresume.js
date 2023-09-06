b2c1fb32af1cc5b22bb228c3168149c96b1e4f0d
Sasi Ganesan <109272446+SasiGanesan@users.noreply.github.com>
2023-09-06 16:24:17 +0530

    Add files via upload
    updated and using html , css and java script with popup window using name change in html file.

--- sasiresume.js
+++ sasiresume.js
@@ -1,21 +1,20 @@
 const editButton = document.getElementById("editButton");
-
-const popupcontainer = document.getElementById("popup-container");
+const popup = document.getElementById("popup");
 const firstIn = document.getElementById("fname");
 const lastIn = document.getElementById("lname");
 const ProfessionIn = document.getElementById("profession");
 
 editButton.addEventListener("click", () => {
-  popupcontainer.style.visibility = "visible";
-  firstIn.value = document.querySelector(".style1").textContent;
-  lastIn.value = document.querySelector(".style2").textContent;
-  ProfessionIn.value = document.querySelector(".pro").textContent;
+  popup.style.display = "flex";
+  firstIn.value = document.querySelector("#style1").textContent;
+  lastIn.value = document.querySelector("#style2").textContent;
+  ProfessionIn.value = document.querySelector("#pro").textContent;
 });
 
 const saveButton = document.getElementById("saveButton");
 saveButton.addEventListener("click", () => {
-  document.querySelector(".style1").textContent = firstIn.value;
-  document.querySelector(".style2").textContent = lastIn.value;
-  document.querySelector(".pro").textContent = ProfessionIn.value;
-  popupcontainer.style.display = "hidden";
+  document.querySelector("#style1").textContent = firstIn.value;
+  document.querySelector("#style2").textContent = lastIn.value;
+  document.querySelector("#pro").textContent = ProfessionIn.value;
+  popup.style.display = "none";
 });
